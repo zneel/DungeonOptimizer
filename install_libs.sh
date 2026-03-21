a254110134ecfc6d442d7c1042f22a0877da6a0e
Elliot Bouvier <elliotbouvier@proton.me>
2026-03-21 11:41:24 +0100

    Fix LibStub download URL - was returning 404
    Fixes #1 - LibStub.lua was a 404 HTML page because the GitHub raw URL
    (WoWUIDev/LibStub) doesn't exist. LibStub is bundled in the Ace3 repo,
    so now we extract it from the same Ace3 tarball we already download.

--- install_libs.sh
+++ install_libs.sh
@@ -12,11 +12,11 @@ mkdir -p "$LIBS_DIR"
 echo "=== Dungeon Optimizer - Installation des librairies ==="
 echo ""
 
-# Ace3 - all modules from one repo
-ACE3_MODULES="AceAddon-3.0 AceConsole-3.0 AceDB-3.0 AceEvent-3.0 AceTimer-3.0 AceGUI-3.0"
+# Ace3 - all modules from one repo (includes LibStub and CallbackHandler)
+ACE3_MODULES="AceAddon-3.0 AceConsole-3.0 AceDB-3.0 AceEvent-3.0 AceTimer-3.0 AceGUI-3.0 CallbackHandler-1.0 LibStub"
 ACE3_URL="https://github.com/WoWUIDev/Ace3/archive/refs/heads/master.tar.gz"
 
-echo "[1/4] Ace3 ($(echo $ACE3_MODULES | wc -w | tr -d ' ') modules)..."
+echo "[1/3] Ace3 ($(echo $ACE3_MODULES | wc -w | tr -d ' ') modules)..."
 TMP=$(mktemp -d)
 curl -sL "$ACE3_URL" -o "$TMP/ace3.tar.gz"
 tar -xzf "$TMP/ace3.tar.gz" -C "$TMP"
@@ -25,23 +25,14 @@ for mod in $ACE3_MODULES; do
     cp -r "$TMP/Ace3-master/$mod" "$LIBS_DIR/$mod"
     echo "  - $mod"
 done
-# CallbackHandler is bundled with Ace3
-rm -rf "$LIBS_DIR/CallbackHandler-1.0"
-cp -r "$TMP/Ace3-master/CallbackHandler-1.0" "$LIBS_DIR/CallbackHandler-1.0"
-echo "  - CallbackHandler-1.0"
 rm -rf "$TMP"
 
-echo "[2/4] LibStub..."
-mkdir -p "$LIBS_DIR/LibStub"
-curl -sL "https://raw.githubusercontent.com/WoWUIDev/LibStub/master/LibStub.lua" \
-    -o "$LIBS_DIR/LibStub/LibStub.lua"
-
-echo "[3/4] LibDataBroker-1.1..."
+echo "[2/3] LibDataBroker-1.1..."
 mkdir -p "$LIBS_DIR/LibDataBroker-1.1"
 curl -sL "https://raw.githubusercontent.com/tekkub/libdatabroker-1-1/master/LibDataBroker-1.1.lua" \
     -o "$LIBS_DIR/LibDataBroker-1.1/LibDataBroker-1.1.lua"
 
-echo "[4/4] LibDBIcon-1.0..."
+echo "[3/3] LibDBIcon-1.0..."
 mkdir -p "$LIBS_DIR/LibDBIcon-1.0"
 curl -sL "https://raw.githubusercontent.com/wowace-clone/LibDBIcon-1.0/master/LibDBIcon-1.0/LibDBIcon-1.0.lua" \
     -o "$LIBS_DIR/LibDBIcon-1.0/LibDBIcon-1.0.lua"
